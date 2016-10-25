2d33b7a3489faa379579f6a2cebe26f670eddc8c
SG <igor1892koval@gmail.com>
2016-10-26 01:01:36 +0300

    First change

--- src/homework/TestClass.java
+++ src/homework/TestClass.java
@@ -1,3 +1,10 @@
+/*
+ * @Main.java
+ *
+ * Copyright(c) Igor Koval
+ */
+
+
 package homework;
 
 import java.io.BufferedReader;
@@ -7,67 +14,69 @@ import java.io.InputStreamReader;
 public class TestClass {
 
     public static void main(String[] args) throws IOException {
+        String phoneNum;
+        boolean flag = false;
+
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        System.out.print("Enter the phone number : ");
-        String phoneNum = reader.readLine();
-        while (!validityCheck(phoneNum)) {
-            System.out.println("Invalid phone number format! Please, try to enter it again in the format 380XXXXXXXXX.");
+        do {
             System.out.print("Enter the phone number : ");
             phoneNum = reader.readLine();
-        }
-        System.out.println("Phone number is correct");
-
-        //calculations
-        int result = 0;
-        int count = 0;
-        for (int i = 0; i < phoneNum.length(); i++) {
-            result += (phoneNum.charAt(i) - '0');
-        }
-        count++;
-        System.out.println("1 part of calculation, sum is -- " + result);
-        while (Integer.toString(result).length() != 1) {
-            result = (Integer.toString(result).charAt(0) - '0') + (Integer.toString(result).charAt(1) - '0');
-            count++;
-            System.out.println(count + " part of calculation, sum is -- " + result);
-        }
-        System.out.print("Final result is ");
-        switch (result) {
-            case 1:
-                System.out.println("One");
-                break;
-            case 2:
-                System.out.println("Two");
-                break;
-            case 3:
-                System.out.println("Three");
-                break;
-            case 4:
-                System.out.println("Four");
-                break;
-            default:
-                System.out.println(result);
-        }
+            if (validityCheck(phoneNum)) {
+                System.out.println("Phone number is correct");
+                int count = 0;
+                while (phoneNum.length() != 1) {
+                    phoneNum = calculateSum(phoneNum);
+                    count++;
+                    System.out.println(count + " part of calculation, sum is -- "
+                            + phoneNum);
+                }
+                System.out.print("Final result is ");
+                switch (phoneNum) {
+                    case "1":
+                        System.out.println("One");
+                        break;
+                    case "2":
+                        System.out.println("Two");
+                        break;
+                    case "3":
+                        System.out.println("Three");
+                        break;
+                    case "4":
+                        System.out.println("Four");
+                        break;
+                    default:
+                        System.out.println(phoneNum);
+                }
+                flag = true;
+            } else {
+                System.out.println("Invalid phone number format!"
+                        + " Please, try to enter it again in the"
+                        + " format 380XXXXXXXXX.");
+            }
+        } while (!flag);
     }
 
-    private static boolean validityCheck(String number) {
-        if (number.length() != 12) {
-            return false;
-        }
+    private static boolean validityCheck(String phoneNumber) {
         String prefix = "";
-        char digit;
-        for (int i = 0; i < number.length(); i++) {
-            digit = number.charAt(i);
-            if (!(digit >= '0' && digit <= '9')) {
+
+        for (int i = 0; i < phoneNumber.length(); i++) {
+            if (!((phoneNumber.charAt(i) >= '0') && (phoneNumber.charAt(i) <= '9'))) {
                 return false;
-            }
-            if (i < 3) {
-                prefix += number.charAt(i);
+            } else if (i < 3) {
+                prefix += phoneNumber.charAt(i);
             }
         }
-        if (!(prefix.equals("380"))) {
-            return false;
+        return !((phoneNumber.length() != 12) || (!(prefix.equals("380"))));
+    }
+
+    private static String calculateSum(String phoneNumber) {
+        int result = 0;
+
+        for (int i = 0; i < phoneNumber.length(); i++) {
+            result += Integer.parseInt(phoneNumber.charAt(i) + "");
         }
-        return true;
+        return Integer.toString(result);
     }
 }
+
 
